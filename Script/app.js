ce3f20634bdce5fb855eb7a9b08aedc553f4dc17
BrianaQuintero <a18490969@itmexicali.edu.mx>
2021-12-02 19:53:20 -0800

    nuevaslineas

--- Script/app.js
+++ Script/app.js
@@ -8,9 +8,11 @@ $(function() {
   $(".toggle").on("click", function() {
     if ($(".item").hasClass("active")) {
         $(".item").removeClass("active");
+        $(".h1").css("display", "block");
         $(this).find("a").html("<i class='fas fa-bars'></i>");
     } else {
         $(".item").addClass("active");
+        $(".h1").css("display", "none");
         $(this).find("a").html("<i class='fas fa-times'></i>");
     }
 });
